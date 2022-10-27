035d620d8d41a6aceb0963789ea3441dcbb81e42
Ranjith101 <89287908+Ranjith101@users.noreply.github.com>
2022-10-27 11:30:25 +0530

    url

--- app.js
+++ app.js
@@ -1,27 +1,42 @@
 // Importing express module
-const express = require('express');
+const express = require("express");
 const app = express();
-const os = require('os');
-
+const os = require("os");
+const PORT = 3000;
 app.use(express.json());
 
-app.get('/', (req, res) => {
-res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+app.post("*", function (req, res) {
+  const { username, password } = req.body;
+  const { authorization } = req.headers;
+  const hostName = os.hostname();
+
+  const protocol = req.protocol;
+  const host = req.hostname;
+  const url = req.originalUrl;
+  const port = process.env.PORT || PORT;
+
+  const fullUrl = `${protocol}://${host}:${port}${url}`;
+
+  //  const responseString = `Full URL is: ${fullUrl}`;
+  res.send({
+    hostName,
+    username,
+    password,
+    authorization,
+	fullUrl
+  });
+  console.log(hostName)
+});
+app.listen(PORT, (error) => {
+  // Listen
+  if (!error)
+    console.log(
+      "Server is Successfully Running,and App is listening on port " + PORT
+    );
+  else console.log("Error occurred, server can't start", error);
 });
 
-app.post('/', (req, res) => {
-const { username, password } = req.body;
-const { authorization } = req.headers;
-const hostName = os.hostname();
-res.send({
-	hostName,
-	username,
-	password,
-	authorization,
-});
-// console.log(res);
-});
 
-app.listen(3000, () => {
-console.log('Our express server is up on port 3000');
-});
